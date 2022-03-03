60a3af02aee3247c4fd8e9bb785afccf6b936145
williamyyj <williamyyj@tpe.hyweb.com.tw>
2022-03-03 18:20:11 +0800

    base-line

--- src/main/java/org/cc/cp/CodePush.java
+++ src/main/java/org/cc/cp/CodePush.java
@@ -6,7 +6,7 @@ public class CodePush {
     public static void main(String[] args){
         String base = App.base("baphiq");
         
-        CodePushModel m = new CodePushModel(base, "D:\\hyweb\\codepush\\baphiq\\20211215個資法","webpos");
+        CodePushModel m = new CodePushModel(base, "D:\\hyweb\\codepush\\baphiq\\20220307收貨管理","webpos");
         System.out.println(m.getCfg().toString(4));
         m.execute();
     }
