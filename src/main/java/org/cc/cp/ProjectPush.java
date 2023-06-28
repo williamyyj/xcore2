27890d807ca56157efc06888ff0aea64283e0878
williamyyj <williamyyj@gmail.com>
2023-06-28 17:50:00 +0800

    base-line

--- src/main/java/org/cc/cp/ProjectPush.java
+++ src/main/java/org/cc/cp/ProjectPush.java
@@ -8,13 +8,12 @@ import org.cc.cp.item.JavaPathItem;
 public class ProjectPush {
 
     public static void main(String[] args) throws IOException {
-        // String tp = "D:/HHome/GoogleDrive/myjob/hyweb/農藥/05_問題單/2022/2022新功能2";
         //String tp = "D:/HHome/hyweb/維護/國防部/上版/2023/20230329";
-        //String tp = "D:/HHome/GoogleDrive/myjob/hyweb/農藥/05_問題單/2023/2023新功能";
-    	String tp = "D:/HHome/GoogleDrive/myjob/hyweb/農藥/05_問題單/2023/20230505";
-        //ProjectPushModel prj = new ProjectPushModel(App.project,"baphiq:baphiq",tp,"20230401");
-        ProjectPushModel prj = new ProjectPushModel(App.project,"baphiq:admin",tp,"20230401");
-        //ProjectPushModel prj = new ProjectPushModel(App.project,"wpos:gipWebPos",tp,"20230201");
+        //String tp = "G:/我的雲端硬碟/myjob/hyweb/農藥/05_問題單/2023/2023新功能申請補登補正"; // using 20230501
+    	String tp = "G:/我的雲端硬碟/myjob/hyweb/農藥/05_問題單/2023/20230617"; // using 20230601 
+        ProjectPushModel prj = new ProjectPushModel(App.project,"baphiq:baphiq",tp,"20230601");
+        //ProjectPushModel prj = new ProjectPushModel(App.project,"baphiq:admin",tp,"2023001");
+        //ProjectPushModel prj = new ProjectPushModel(App.project,"wpos:gipWebPos",tp,"20230601");
         //ProjectPushModel prj = new ProjectPushModel(App.project,"baphiqMedia:front",tp,"20221101");
         //ProjectPushModel prj = new ProjectPushModel(App.project,"baphiqMedia:admin",tp,"20221101");
         //ProjectPushModel prj = new ProjectPushModel(App.project,"mnd:mndAdmin",tp,"20230201");
